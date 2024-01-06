90195a5d366cf012caae8d13ad9f97ecf6d1f7a0
PFC <81114960+PFC-developer@users.noreply.github.com>
2024-01-05 18:24:49 -1000

    show client-id and memo fields

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use clap::Arg;
-use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use terra_proto::generated::cosmos::tx::v1beta1::Tx;
-
+use clap::Arg;
+use dotenv::dotenv;
 use prost::Message;
+use serde::{Deserialize, Serialize};
 use terra_proto::generated::cosmos::base::v1beta1::Coin;
 use terra_proto::generated::cosmos::crypto::secp256k1::PubKey;
+use terra_proto::generated::cosmos::tx::v1beta1::Tx;
+use terra_proto::generated::ibc::core::client::v1::MsgUpdateClient;
 //use terra_proto::generated::ibc::applications::transfer::v1::MsgTransfer;
 //use terra_proto::generated::ibc::core::channel::v1::{MsgAcknowledgement, MsgRecvPacket};
 //use terra_proto::generated::ibc::core::client::v1::{MsgCreateClient, MsgUpdateClient};
@@ -21,6 +21,7 @@ pub const STATE_NAME: &'static str = "get_ibc_req.json";
 pub const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 /// NAME of package
 pub const NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
+
 #[derive(Deserialize, Serialize, Debug)]
 pub(crate) struct IBCState {
     pub height: u64,
@@ -30,6 +31,9 @@ pub struct IBCTx {
     pub coins: Vec<Coin>,
     pub account: String,
     pub sequence: u64,
+    pub client: String,
+    pub memo: String,
+
 }
 
 fn process_tx(hex: &str) -> anyhow::Result<Option<IBCTx>> {
@@ -38,6 +42,7 @@ fn process_tx(hex: &str) -> anyhow::Result<Option<IBCTx>> {
     let tx: Tx = Tx::decode(b64_decoded.as_slice())?;
 
     if let Some(body) = tx.body {
+        let mut client: String = String::from("-");
         let mut has_ibc = false;
         for msg in body.messages {
             /*
@@ -91,10 +96,17 @@ fn process_tx(hex: &str) -> anyhow::Result<Option<IBCTx>> {
 
             */
             // Transfers are not paid by relayers. so ignore them.
-            if msg.type_url.as_str().starts_with("/ibc")
-                && msg.type_url != "/ibc.applications.transfer.v1.MsgTransfer"
-            {
-                has_ibc = true
+            if msg.type_url.as_str().starts_with("/ibc") {
+                if msg.type_url == "/ibc.core.client.v1.MsgUpdateClient" {
+                    let upd = MsgUpdateClient::decode(msg.value.as_slice()).unwrap();
+                    println!("ClientID:{}", upd.client_id);
+                    client = upd.client_id.clone();
+                }
+
+                if msg.type_url != "/ibc.applications.transfer.v1.MsgTransfer"
+                {
+                    has_ibc = true;
+                }
             }
         }
         if has_ibc {
@@ -115,14 +127,16 @@ fn process_tx(hex: &str) -> anyhow::Result<Option<IBCTx>> {
                     // only first signer gets credit.
                     if let Some(s) = auth.signer_infos.first() {
                         if let Some(pubkey) = &s.public_key {
-                            let pub_key_p :terra_proto::generated::cosmos::crypto::secp256k1::PubKey= PubKey::decode(pubkey.value.as_slice()).unwrap();
+                            let pub_key_p: terra_proto::generated::cosmos::crypto::secp256k1::PubKey = PubKey::decode(pubkey.value.as_slice()).unwrap();
                             let account = PublicKey::from_public_key(pub_key_p.key.as_slice());
 
-                            log::debug!("Account:{} {}", account.account()?, s.sequence);
+                            log::debug!("Account:{} {} {}", account.account()?, s.sequence,&client);
                             let ibc = IBCTx {
                                 coins: fee.amount,
                                 account: account.account()?,
                                 sequence: s.sequence,
+                                client,
+                                memo: body.memo.clone(),
                             };
                             return Ok(Some(ibc));
                         }
@@ -134,7 +148,8 @@ fn process_tx(hex: &str) -> anyhow::Result<Option<IBCTx>> {
 
     Ok(None)
 }
-fn process_block(txs: &Vec<String>) -> anyhow::Result<HashMap<String, HashMap<String, u128>>> {
+
+fn process_block(block_height: u64, txs: &Vec<String>) -> anyhow::Result<HashMap<String, HashMap<String, u128>>> {
     let mut tally: HashMap<String, HashMap<String, u128>> = Default::default();
     for tx in txs {
         if let Some(ibc) = process_tx(tx)? {
@@ -143,6 +158,9 @@ fn process_block(txs: &Vec<String>) -> anyhow::Result<HashMap<String, HashMap<St
                 .iter()
                 .map(|x| (x.denom.clone(), x.amount.parse::<u128>().unwrap()))
                 .collect::<HashMap<String, u128>>();
+            let coinstr = coins.iter().map(|x| { format!("{}{}", x.1, x.0) }).collect::<Vec<String>>();
+
+            println!("{},{},{},{},{}", block_height, ibc.account, coinstr.join("|"), ibc.client, ibc.memo);
 
             tally
                 .entry(ibc.account)
@@ -158,13 +176,14 @@ fn process_block(txs: &Vec<String>) -> anyhow::Result<HashMap<String, HashMap<St
     }
     Ok(tally)
 }
+
 async fn fetch_blocks(terra: &Terra, rpc: &str) -> anyhow::Result<IBCState> {
     let mut state: IBCState = if let Ok(file) = std::fs::File::open(STATE_NAME) {
         serde_json::from_reader(file)?
         //IBCState { height: 4988972 }
     } else {
         log::info!("Starting from origin IBC block");
-        IBCState { height: 4985676 }
+        IBCState { height: 8397604 }
         //IBCState { height: 4988972 }
     };
 
@@ -177,7 +196,7 @@ async fn fetch_blocks(terra: &Terra, rpc: &str) -> anyhow::Result<IBCState> {
             let block = terra.rpc(&rpc).block_at_height(state.height).await?;
             let block_height = block.block.header.height;
             let txs = &block.block.data.txs.unwrap_or(vec![]);
-            let block_tally = process_block(txs)?;
+            let block_tally = process_block(block_height, txs)?;
 
             // process_block_emit(block_height, txs, true)?;
             //let block_results: BlockResultsResult =                terra.rpc(rpc).block_results_at_height(state.height).await?;
@@ -197,7 +216,7 @@ async fn fetch_blocks(terra: &Terra, rpc: &str) -> anyhow::Result<IBCState> {
                         .map(|x| format!("{}{}", x.1, x.0))
                         .collect::<Vec<_>>()
                         .join(",");
-                    println!("{},{},{}", block_height, entry.0, coins_str)
+                    //         println!("{},{},{}", block_height, entry.0, coins_str)
                 }
                 log::debug!(
                     "Processing Block {}/ HEAD={} -- {} {:?}",
@@ -215,6 +234,7 @@ async fn fetch_blocks(terra: &Terra, rpc: &str) -> anyhow::Result<IBCState> {
     }
     Ok(state)
 }
+
 async fn run() -> anyhow::Result<()> {
     let cli = cli_helpers::gen_cli_read_only("get_ibc_reqs", "get_ibc_reqs")
         .arg(
@@ -230,10 +250,11 @@ async fn run() -> anyhow::Result<()> {
     log::info!("RPC={}", rpc);
 
     //    let json: serde_json::Value = serde_json::from_str(json_str)?;
-    println!("{},{},{}", "block", "account", "fees");
+    println!("{},{},{},{},{}", "block", "account", "fees", "src_port", "src_channel");
     fetch_blocks(&terra, rpc).await?;
     Ok(())
 }
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
